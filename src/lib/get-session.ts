6bc74d25e778e4c1ac4e5010861f1c9cc1dd014f
ayushx20-dev <ayushhjii16@email.com>
2026-05-25 23:56:39 +0530

    Update project structure

--- src/lib/get-session.ts
+++
@@ -1,9 +0,0 @@
-import { authConfig } from "@/auth";
-import { getRequest } from "@tanstack/react-start/server";
-import { getSession } from "start-authjs";
-
-export async function getCurrentSession() {
-  const request = getRequest();
-
-  return getSession(request, authConfig);
-}
