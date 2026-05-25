8d10a406a7548dc945e81e0ee1b5da4780fa041e
ayushx20-dev <ayushhjii16@email.com>
2026-05-25 18:29:46 +0530

    chore: untrack build and cache files

--- dist/server/assets/get-session-BGEPhOSi.js
+++
@@ -1,9 +0,0 @@
-import { i as getRequest } from "../server.js";
-import { t as authConfig } from "./auth-B3DFAshx.js";
-import { getSession } from "start-authjs";
-//#region src/lib/get-session.ts
-async function getCurrentSession() {
-	return getSession(getRequest(), authConfig);
-}
-//#endregion
-export { getCurrentSession as t };
